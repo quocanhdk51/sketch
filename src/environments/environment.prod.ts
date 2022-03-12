21fd85886697b5f737398df760e4ad0b72a385fe
quocanhdk51 <quocanhdk51@gmail.com>
2022-03-13 02:39:32 +0700

    Build 3 (0313220239)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   config: {
-    appURL: 'http://sketch-api.azurewebsites.net/api/v1/sketch/',
+    appURL: 'https://sketch-api.azurewebsites.net/api/v1/sketch/',
     azureUrl: 'https://login.microsoftonline.com/',
     clientId: '12b65564-3f8d-45aa-8989-d0ba296d0186',
     tenantId: 'e850b2f8-7df2-4859-a864-565be726e8a4',
