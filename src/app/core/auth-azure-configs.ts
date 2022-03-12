32678e6164944ed7acd0aa44984980572aa0059c
quocanhdk51 <quocanhdk51@gmail.com>
2022-03-13 00:24:22 +0700

    Build 2 (1303220024)

--- src/app/core/auth-azure-configs.ts
+++ src/app/core/auth-azure-configs.ts
@@ -4,8 +4,8 @@ export const AZURE_AUTH_CONFIG = {
   Authority: environment.config.azureUrl + environment.config.tenantId,
   ClientId: environment.config.clientId,
   ProtectedResourceMap: 'https://graph.microsoft.com/v1.0/me',
-  PostLogoutRedirectUri: origin,
-  RedirectUri: window.location.origin,
+  PostLogoutRedirectUri: origin + '/sketch/',
+  RedirectUri: window.location.origin  + '/sketch/',
   UserScope: 'user.read',
   AppIDUrl: environment.config.appIdUrl
 };
